dd1d3d2d6fd5759907186134abbb361d6f1da3ee
Nanako-Yamakawa <nanako0118ny@gmail.com>
2024-09-06 14:24:07 +0900

    ブログ投稿機能作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
@@ -11,8 +12,24 @@ class PostController extends Controller
  
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
-   
+   public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
+    public function create()
+{
+    return view('posts.create');
+}
+
+    public function store(PostRequest $request, Post $post)
+{
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+}
 }
 
